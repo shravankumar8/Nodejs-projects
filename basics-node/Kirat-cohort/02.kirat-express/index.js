93b2952c5857f62c99ae8ec0e26e48c9e49ddb09
shravankumar8 <kumashravan5@gmail.com>
2024-01-14 23:09:06 +0530

    fixed couple of problems

--- basics-node/Kirat-cohort/02.kirat-express/index.js
+++ basics-node/Kirat-cohort/02.kirat-express/index.js
@@ -1,29 +1,37 @@
-const express= require("express");
-const bodyparser=require("body-parser");
-var app= express();
-app.use(bodyparser.json())
-port =3000
-function calculate(n){
-    var sum=0
-    for(var i=0; i < n; i++){
-        sum=sum+i
-    }
-   
-    return sum
+
+const express = require("express");
+var app = express();
+
+var bodyParser = require("body-parser");
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+port = 3000;
+function calculate(n) {
+  var sum = 0;
+  for (var i = 0; i < n; i++) {
+    sum = sum + i;
+  }
+
+  return sum;
 }
-function handleFirstRequest(req, res){
-    counter=req.query.counter
-    
-    // console.log("the query params are",req.query)
-    
-    // console.log("the headers are",req.headers)
-    var calculatedsum = calculate(counter);
-    
-    var answer=counter+" for this the sum  is "+calculatedsum
-    res.send(answer)
+function handleFirstRequest(req, res) {
+  counter = req.body.counter;
+  console.log(req)
+  console.log("the query params are",req.query)
+
+//   console.log("the headers are",req.headers)
+  var calculatedsum = calculate(counter);
+
+  var answer = counter + " for this the sum  is " + calculatedsum;
+  res.send(answer);
 }
-app.get("/", handleFirstRequest)
-function started(){
-    console.log("started")
+app.post("/", handleFirstRequest);
+function started() {
+  console.log("started");
 }
-app.listen(port,started)
+app.listen(port, started);
