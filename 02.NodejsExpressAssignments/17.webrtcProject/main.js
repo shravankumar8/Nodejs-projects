df843bba136fd763cdef0b0ddee6a8156c9780a5
shravankumar8 <kumashravan5@gmail.com>
2024-02-04 11:30:50 +0530

    updated

---
+++ 02.NodejsExpressAssignments/17.webrtcProject/main.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const { createServer } = require("node:http");
+const { join } = require("node:path");
+const app = express();
+const server = createServer(app);
+
+app.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "index.html"));
+});
+
+server.listen(3000, "192.168.29.52", () => {
+  console.log("server running at http://localhost:3000");
+});
