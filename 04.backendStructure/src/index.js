11269de4e8de1e0ea438314f30139e95ba2d3fe9
shravankumar8 <kumashravan5@gmail.com>
2024-02-21 19:55:33 +0530

    updated bro

--- 04.backendStructure/src/index.js
+++ 04.backendStructure/src/index.js
@@ -1,6 +1,20 @@
+import express from "express";
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
-dotenv.config({
-  path: "../.env.sample",
-});
-connectDB();
+import cors from 'cors'
+const app = express();
+dotenv.config({});
+connectDB()
+.then(() => {
+  app.use(cors({
+    origin:process.env.CORSORIGIN,
+    credentials:true
+  }))
+  app.use(express.json({limit:"16kb"} ))//usually by default you can read the json inputs instead youd use body parser to parse and 
+  //read but now express provides with inbuilt module to parse the body
+//limit is used to limit the byes of input data 
+  
+  })
+  .catch((err) => {
+    console.error("db connect failed ", err);
+  });
